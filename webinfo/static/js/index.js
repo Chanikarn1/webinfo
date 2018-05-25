2086b8e3c7e6fb08540a3635d45ce60fa881597b
unknown <jidapa.dumrongsombat@gmail.com>
2018-05-25 17:23:36 +0700

    edit gallery

--- webinfo/static/js/index.js
+++ webinfo/static/js/index.js
@@ -55,34 +55,10 @@ function myfunction(x){
     } else {
         a.style.color = "red";
     }
-    // x.classList.toggle("fa fa-heart");
-    }
+}
 
 
-    var slideIndex = 1;
-    showDivs(slideIndex);
     
-    function plusDivs(n) {
-      showDivs(slideIndex += n);
-    }
-    
-    function showDivs(n) {
-      var i;
-      var y = document.getElementsByClassName("mySlides2");
-      var x = document.getElementsByClassName("mySlides");
-      var z = document.getElementsByClassName("mySlides3");
-      if (n > x.length) {slideIndex = 1}    
-      if (n < 1) {slideIndex = x.length}
-      for (i = 0; i < x.length; i++) {
-         
-         x[i].style.display = "none"; 
-         y[i].style.display = "none"; 
-         z[i].style.display = "none"; 
-      }
-      x[slideIndex-1].style.display = "inline";
-      y[slideIndex-1].style.display = "inline"; 
-      z[slideIndex-1].style.display = "inline"; 
-    }
 
 
 
